c8c3669e395a940453668481e20256a0537aa164
Tania <tbarrosolopez@gmail.com>
2017-11-27 12:50:02 -0500

    register.component - Material Design

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {User } from '../../_models/user';
+import {User} from '../../_models/user';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -11,6 +12,8 @@ export class RegisterComponent implements OnInit {
     user: User;
     loading: boolean;
     error: string;
+    registerForm: FormGroup;
+    hide = true;
 
     constructor() {
         this.user = new User();
@@ -19,6 +22,25 @@ export class RegisterComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.createForm();
+    }
+
+    private createForm() {
+        this.registerForm = new FormGroup({
+            name: new FormControl('', Validators.required),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', Validators.required),
+            confirmpassword: new FormControl('', Validators.required),
+        });
+    }
+
+    getErrorMessage() {
+        // console.log(this.registerForm.controls['confirmpassword'].hasError('validateEqual'));
+        return this.registerForm.controls['name'].hasError('required') ? 'You must enter a value' :
+            this.registerForm.controls['email'].hasError('required') ? 'You must enter a value' :
+                this.registerForm.controls['email'].hasError('email') ? 'Not a valid email' :
+                    this.registerForm.controls['password'].hasError('required') ? 'You must enter a value' :
+                        '';
     }
 
     register() {
