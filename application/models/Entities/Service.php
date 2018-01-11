0f7f9189136b4db238098592d8398be36ea028bb
Yoidel <yoidel86@gmail.com>
2018-01-11 15:10:11 -0500

    Merge branches 'master' and 'plantilla' of https://github.com/yasielelreyglez/services
    Conflicts:
    	application/controllers/Client.php
    	application/views/client/_layouts/login.php
    	application/views/client/_partials/toolbar.php

--- application/models/Entities/Service.php
+++ application/models/Entities/Service.php
@@ -691,6 +691,7 @@ namespace Entities {
                 file_put_contents($path, base64_decode($icon['value']));
                 $image = new Image();
                 $image->setTitle($save_path);
+                createThumb($save_path,200,250);
                 $this->addImage($image);
             }
             return $this;
@@ -1008,6 +1009,132 @@ namespace Entities {
         public function setTodopais($todopais)
         {
             $this->todopais = $todopais;
+        }
+        function createThumb($sImagen, $nWidth = false, $nHeight = false)
+        {
+            // Variables
+            $sNombre = null;
+            $sPath = null;
+            $sExt = null;
+            $aImage = null;
+            $aThumb = null;
+            $aImageMarco = null;
+            $ImTransparente = null;
+            $aSize = null;
+            $nWidthMarco = false;
+            $nWidthHeight = false;
+            $nX = false;
+            $nY = false;
+
+            // Obtenemos el nombre de la imagen
+            $sNombre = basename( $sImagen );
+            // Obtenemos la ruta especificada para buscar la imagen
+            $sPath = dirname( $sImagen ) . '/';
+            // Obtenemos la extension de la imagen
+            $sExt = mime_content_type( $sImagen );
+
+            // Creamos el directorio thumbs
+            if( ! is_dir( $sPath . 'thumbs/' ) )
+                @mkdir( $sPath . 'thumbs/', 0777, true ) or die( 'No se ha podido crear el directorio "' . $sPath . 'thumbs/".' );
+
+            // Creamos la imagen a partir del tipo
+            switch( $sExt )
+            {
+                // Imagen JPG
+                case 'image/jpeg':
+                    $aImage = @imageCreateFromJpeg( $sImagen );
+                    break;
+                // Imagen GIF
+                case 'image/gif':
+                    $aImage = @imageCreateFromGif( $sImagen );
+                    break;
+                // Imagen PNG
+                case 'image/png':
+                    $aImage = @imageCreateFromPng( $sImagen );
+                    break;
+                // Imagen BMP
+                case 'image/wbmp':
+                    $aImage = @imageCreateFromWbmp( $sImagen );
+                    break;
+                default:
+                    return 'No se conoce el tipo de imagen enviado, por favor cambie el formato. Sólo se permiten imágenes *.jpg, *.gif, *.png ó *.bmp.';
+                    break;
+            }
+
+            // Obtenemos el tamaño de la imagen original
+            $aSize = getImageSize( $sImagen );
+
+            // Calculamos las proporciones de la imagen //
+
+            // Obteniendo el alto (Recogiendo ancho y no alto)
+            if( $nWidth !== false && $nHeight === false )
+                $nHeight = round( ( $aSize[1] * $nWidth ) / $aSize[0] );
+            // Obteniendo el ancho (Recogiendo alto y no ancho)
+            elseif( $nWidth === false && $nHeight !== false )
+                $nWidth = round( ( $aSize[0] * $nHeight ) / $aSize[1] );
+            // Obteniendo proporciones (Recogiendo alto y ancho)
+            elseif( $nWidth !== false && $nHeight !== false )
+            {
+                // Guardamos las dimensiones del marco
+                $nWidthMarco = $nWidth;
+                $nHeightMarco = $nHeight;
+
+                // Si el ancho es mayor
+                if( $nWidth < $nHeight )
+                {
+                    $nHeight = round( ( $aSize[1] * $nWidth ) / $aSize[0] );
+                    $nX = 0;
+                    $nY = round( ( $nHeightMarco - $nHeight ) / 2 );
+                }
+                // Si el alto es mayor
+                elseif( $nHeight < $nWidth )
+                {
+                    $nWidth = round( ( $aSize[0] * $nHeight ) / $aSize[1] );
+                    $nX = round( ( $nWidthMarco - $nWidth ) / 2 );;
+                    $nY = 0;
+                }
+            }
+            // El ancho y el alto no se han enviado, informamos del error
+            elseif( $nWidth === false && $nHeight === false )
+                return 'No se ha especificado ningún valor para el ancho y el alto de la imágen.';
+
+            // La nueva imagen reescalada
+            $aThumb = imageCreateTrueColor( $nWidth, $nHeight );
+
+            // Reescalamos
+            imageCopyResampled( $aThumb, $aImage, 0, 0, 0, 0, $nWidth, $nHeight, $aSize[0], $aSize[1] );
+
+            // Si tenemos que crear el marco
+            if( $nWidthMarco !== false && $nHeightMarco !== false )
+            {
+                // El marco
+                $aImageMarco = imageCreateTrueColor( $nWidthMarco, $nHeightMarco );
+
+                // Establecemos la imagen de fondo transparente
+                imageAlphaBlending( $aImageMarco, false );
+                imageSaveAlpha( $aImageMarco, true );
+
+                // Establecemos el color transparente (negro)
+                $ImTransparente = imageColorAllocateAlpha( $aImageMarco, 0, 0, 0, 0xff/2 );
+
+                // Ponemos el fondo transparente
+                imageFilledRectangle( $aImageMarco, 0, 0, $nWidthMarco, $nHeightMarco, $ImTransparente );
+
+                // Combinamos las imagenes
+                imageCopyResampled( $aImageMarco, $aThumb, $nX, $nY, 0, 0, $nWidth, $nHeight, $nWidth, $nHeight );
+
+                // Cambiamos la instancia
+                $aThumb = $aImageMarco;
+            }
+
+            // Salvamos
+            imagePng( $aThumb, $sPath . 'thumbs/' . $sNombre );
+
+            // Liberamos
+            imageDestroy( $aImage );
+            imageDestroy( $aThumb );
+
+            return true;
         }
 
     }
